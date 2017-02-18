2fdac7db83078c0fca946cff117a17629f852518
Kartikey Kushwaha <kartikey92@gmail.com>
2017-02-18 18:23:54 +0530

    #13 seems to be fixed for case of . More testing required.

--- lineartimer/src/main/java/io/github/krtkush/lineartimer/LinearTimerView.java
+++ lineartimer/src/main/java/io/github/krtkush/lineartimer/LinearTimerView.java
@@ -69,10 +69,8 @@ public class LinearTimerView extends View {
         rectF = new RectF(
                 (int) convertDpIntoPixel(strokeWidthInDp),
                 (int) convertDpIntoPixel(strokeWidthInDp),
-                (int) convertDpIntoPixel(circleRadiusInDp * 2)
-                        + (int) convertDpIntoPixel(strokeWidthInDp),
-                (int) convertDpIntoPixel(circleRadiusInDp * 2)
-                        + (int) convertDpIntoPixel(strokeWidthInDp));
+                (int) convertDpIntoPixel(circleRadiusInDp * 2) + (int) convertDpIntoPixel(strokeWidthInDp),
+                (int) convertDpIntoPixel(circleRadiusInDp * 2) + (int) convertDpIntoPixel(strokeWidthInDp));
 
         arcPaint = new Paint();
         arcPaint.setAntiAlias(true);
@@ -100,6 +98,54 @@ public class LinearTimerView extends View {
         invalidate();
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int desiredWidth = (int) convertDpIntoPixel(circleRadiusInDp)
+                + (int) convertDpIntoPixel(strokeWidthInDp);
+        int desiredHeight = (int) convertDpIntoPixel(circleRadiusInDp)
+                + (int) convertDpIntoPixel(strokeWidthInDp);
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int width;
+        int height;
+
+        //Measure Width
+        if (widthMode == MeasureSpec.EXACTLY) {
+            //Must be this size
+            width = widthSize;
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            width = Math.min(desiredWidth, widthSize);
+        } else {
+            //Be whatever you want
+            width = desiredWidth;
+        }
+
+        //Measure Height
+        if (heightMode == MeasureSpec.EXACTLY) {
+            //Must be this size
+            height = heightSize;
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            height = Math.min(desiredHeight, heightSize);
+        } else {
+            //Be whatever you want
+            height = desiredHeight;
+        }
+
+        //MUST CALL THIS
+        setMeasuredDimension(width * 2, height * 2);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
 
     /**
      * Method to get the degrees up-till which the arc is already pre-filled.
